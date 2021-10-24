4b7dbfccf6458ed500fb3273ce96f50428a5ea8f
Nikhil Tyagi <nikhiltyagi@live.com>
2021-10-24 15:40:16 +0530

    removedmanusa page

--- door-automation.php
+++ door-automation.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -8,24 +7,29 @@
     <meta name="description" content="Themeforest Template Polo, html template">
     <link rel="icon" type="image/png" href="images/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Document title -->
-    <title>POLO | The Multi-Purpose HTML5 Template</title>
-    <!-- Stylesheets & Fonts -->
+    <title>Dhingra group | Door Automation</title>
     <link href="css/plugins.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
-<?php include('include/top-bar.php'); ?>
+<?php include('include/topbar.php'); ?>    
 <?php include('include/header.php');?>
         <section class="division" id="page-title" style="background-image:linear-gradient(135deg,#0000008a ,#0000ff2e),url(images/mass.jpg)">
             <div class="container">
                 <div class="page-title">
                     <h1>Door Automation</h1>
-                   
                 </div>
                 </div>
         </section>                         
+                    <!-- Product additional tabs -->
+                       <!-- <section id="page-content">
+                        <div class="container">
+                          
+                            <p class="pl-40">MASS TRANSIT OR PUBLIC TRANSPORTATION IS THE MOVEMENT OF PEOPLE WITHIN URBAN AREAS, INTERCITY AND INTER COUNTRY USING GROUP TRAVEL TECHNOLOGIES SUCH AS BUSES AND TRAINS, METRO'S, TRAMS & AIRCRAFTS.</p>
+                            <p>  However transporting a lot of people at a time has always remained a challenge for all modes of transport therefore proper control keeping in view the safety, security and convenience. This is where LongLasst Entrada comes in with various automation solutions that support the city infrastructure for managing public transportation.</p>
+                        </div>
+                    </section> -->
                     <section id="page-content" class="main-section">
                         <div class="container">
                           <div class="row">
@@ -53,6 +57,7 @@
               </div>
               <div class="col-md-6 text-muted">
                 <div class="card-body">
+                  <!-- <h5 class="card-title">Card title</h5> -->
                   <div class="elementor-widget-container" >
                     <p>The Visio+ operator range is made up of three versions of drive unit: Visio+ 100, Visio+ 125 and Visio+ 125HD, the features and measurements of which adapt to all construction requirements. It’s compatible with the entire range of Manusa door leaves and other commercial door leaves. It incorpores the IoT technology to control connectivity and remote control of the door.</p><p>It also offers a series of additional functions through mobile apps for both users and the Technical Service. Manusa Visio+ operator range have certificate of durability for 1 million cycles according to Standard UNE 16361:2014+A1:2017</p>	
                     <div class="click-btn">
@@ -62,16 +67,23 @@
               </div>
             </div>
           </div>
+           
     </div>
 </div>
+                                 
 </div>
+
+
+                            
                    <div class="row" id="btn1">
                     <div class="col-lg-12">
+                
                         <div class="section-title text-center">
                             <h2 class="elementor-heading-title elementor-size-default py-4">Activa <span class="note">+</span></h2>
                            </div>
-                           <div class="card">
+                           <div class="card " >
                             <div class="row g-0 ">
+                             
                               <div class="col-md-6 text-muted">
                                 <div class="card-body">
                                   <div class="elementor-widget-container" >
@@ -89,9 +101,9 @@
                     </div>
                 </div>
               </div>
-            </div>
-     </section>
-    <?php include('include/footer.php'); ?>
+        </div>
+      </section>
+    <?php include('include/footer.php'); ?>   
     <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
     <script src="js/jquery.js"></script>
     <script src="js/plugins.js"></script>
